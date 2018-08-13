b94633c45e88feb9e39469aee352747343eeaf24
Tibo442 <datwiliknietzeggen+git@gmail.com>
2018-08-13 02:16:41 +0200

    Init

---
+++ Inject.js
@@ -0,0 +1,80 @@
+var transList = {}, updateCount = 0;
+function translate() {
+    walk(document.body);
+    setTimeout(translate, 50);
+}
+function walk(node) {
+    var child, next;
+    switch (node.nodeType) {
+    case 1: // Element
+    case 9: // Document
+    case 11: // Document fragment
+        child = node.firstChild;
+        while (child) {
+            next = child.nextSibling;
+            walk(child);
+            handleTitle(child);
+            child = next;
+        }
+        break;
+    case 3: // Text node
+        if (node.parentElement.tagName.toLowerCase() != "script")
+            handleText(node);
+        break;
+    }
+}
+function handleText(textNode) {
+    if(textNode.isHandeled != updateCount) {
+        let v = translateText(textNode.nodeValue)
+        if(textNode.nodeValue !== v)
+            textNode.nodeValue = v;
+        textNode.isHandeled = updateCount;
+    }
+}
+function handleTitle(titleNode) {
+    if(titleNode.isHandeled != updateCount && titleNode.title) {
+        let v = translateText(titleNode.title)
+        if(titleNode.title !== v)
+            titleNode.title = v;
+        titleNode.isHandeled = updateCount;
+    }
+}
+function translateText(v) {
+    let prevProp = [""];
+    for (var prop of Object.keys(transList).sort((a,b) => b.length - a.length)) {
+        var strippedV = v;
+        for (var i = 0; i < prevProp.length; i++) 
+            strippedV = strippedV.replace(prevProp[i], "");
+        if (/*transList.hasOwnProperty(prop) && v.includes(prop) && */ !v.split(prop)[0].match(/ \($/) && strippedV.includes(prop)) {
+            v = v.replace(prop, transList[prop])
+            prevProp.push(prop);
+        }
+    }
+    return v;
+}
+function download(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE && xmlhttp.status == 200) {
+            if(callback)
+                callback(JSON.parse(xmlhttp.responseText));
+
+            let newData = JSON.parse(xmlhttp.responseText);
+            Object.keys(newData).forEach((k) => transList[k] = newData[k]);
+            if(!updateCount)
+                translate();
+            updateCount++;
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+download('https://raw.githubusercontent.com/KC3Kai/kc3-translations/master/data/en/items.json', function(data) {
+    gItems.forEach((a) => a.name = data[a.name]||a.name)
+});
+download('https://raw.githubusercontent.com/KC3Kai/kc3-translations/master/data/en/ships.json', function(data) {
+    gShips.forEach(function(a) {
+        a.name = data[a.name]||a.name
+    })
+});
+download('URL');
