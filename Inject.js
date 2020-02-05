f2b7d6033d5b0ae0ffbb2c2f7157e930ebd59704
Tibo <Tibowl@users.noreply.github.com>
2020-02-05 15:25:21 +0100

    Add better regex support

--- Inject.js
+++ Inject.js
@@ -2,11 +2,14 @@ let transList = {},
     downloadCount = 0,
     sorted = []
 
+const printTime = false
 function translate() {
-    // const start = new Date().getTime()
+    const start = new Date().getTime()
     walk(document.body)
-    // const end = new Date().getTime()
-    // console.log("Time:", end - start)
+    const end = new Date().getTime()
+
+    if(printTime)
+        console.log("Time:", end - start)
 
     setTimeout(translate, 100)
 }
@@ -49,28 +52,22 @@ function handle(node, key) {
 
 function translateText(line) {
     let stripped = line // Prevents recursive TLs
-    for (let key of sorted)
-        if (stripped.includes(key)) {
-            const regex = new RegExp(key)
-            regex.global = true
-
-            line = line.replace(regex, transList[key])
-            stripped = stripped.replace(regex, "") 
+    for (let entry of sorted)
+        if (entry.regex.test(stripped)) {
+            line = line.replace(entry.regex, entry.translated)
+            stripped = stripped.replace(entry.regex, "")
         }
     return line
 }
 
-function download(url, callback) {
+function download(url, escape, callback) {
     const xmlhttp = new XMLHttpRequest()
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == XMLHttpRequest.DONE && xmlhttp.status == 200) {
-            if (callback)
-                callback(JSON.parse(xmlhttp.responseText))
-
             const newData = JSON.parse(xmlhttp.responseText)
-            Object.entries(newData).forEach((k) => transList[k[0]] = k[1])
-            sorted = Object.keys(transList).sort((a, b) => b.length - a.length)
-
+            if (callback)
+                callback(newData)
+            addData(newData, escape)
             if (downloadCount == 2)
                 translate()
             downloadCount++
@@ -78,8 +75,28 @@ function download(url, callback) {
     }
     xmlhttp.open("GET", url, true)
     xmlhttp.send()
+
 }
 
-download('https://raw.githubusercontent.com/KC3Kai/kc3-translations/master/data/en/items.json', (data) => window.gItems && gItems.forEach((a) => a.name = data[a.name] || a.name))
-download('https://raw.githubusercontent.com/KC3Kai/kc3-translations/master/data/en/ships.json', (data) => window.gShips && gShips.forEach((a) => Object.keys(data).sort((a, b) => b.length - a.length).forEach((b) => a.name = a.name.replace(b, data[b]))))
-download('https://raw.githubusercontent.com/Tibowl/KCToolTL/master/Translations.json?v=' + new Date().getTime())
+function addData(newData, escape) {
+    Object.entries(newData).forEach((k) => transList[escape ? escapeRegExp(k[0]) : k[0]] = k[1])
+    sorted = Object.keys(transList)
+        .sort((a, b) => b.length - a.length)
+        .map(key => {
+            const regex = new RegExp(key)
+            regex.global = true
+            return {
+                regex,
+                translated: transList[key]
+            }
+        })
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#Escaping
+function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+}
+
+download('https://raw.githubusercontent.com/KC3Kai/kc3-translations/master/data/en/items.json', true, (data) => window.gItems && gItems.forEach((a) => a.name = data[a.name] || a.name))
+download('https://raw.githubusercontent.com/KC3Kai/kc3-translations/master/data/en/ships.json', true, (data) => window.gShips && gShips.forEach((a) => Object.keys(data).sort((a, b) => b.length - a.length).forEach((b) => a.name = a.name.replace(b, data[b]))))
+download('https://raw.githubusercontent.com/Tibowl/KCToolTL/master/Translations.json?v=' + new Date().getTime(), false)
