adc24d37ba2d2955ff610f915b9f934cf8707bf4
Tibo <Tibowl@users.noreply.github.com>
2020-02-05 01:27:28 +0100

    Optimize script (up to 10x faster depending on page)

--- Inject.js
+++ Inject.js
@@ -1,83 +1,85 @@
-var transList = {}, updateCount = 0;
+let transList = {},
+    downloadCount = 0,
+    sorted = []
+
 function translate() {
-    walk(document.body);
-    setTimeout(translate, 50);
+    // const start = new Date().getTime()
+    walk(document.body)
+    // const end = new Date().getTime()
+    // console.log("Time:", end - start)
+
+    setTimeout(translate, 100)
 }
+
 function walk(node) {
-    var child, next;
+    let child, next
     switch (node.nodeType) {
-    case 1: // Element
-    case 9: // Document
-    case 11: // Document fragment
-        child = node.firstChild;
-        while (child) {
-            next = child.nextSibling;
-            walk(child);
-            handleTitle(child);
-            child = next;
-        }
-        break;
-    case 3: // Text node
-        if (node.parentElement.tagName.toLowerCase() != "script")
-            handleText(node);
-        break;
+        case 1: // Element
+        case 9: // Document
+        case 11: // Document fragment
+            child = node.firstChild
+            while (child) {
+                next = child.nextSibling
+                walk(child)
+                if (child.lastHandled !== downloadCount) {
+                    handle(child, "title")
+                    handle(child, "label")
+                    child.lastHandled = downloadCount
+                }
+                child = next
+            }
+            break
+        case 3: // Text node
+            if (node.parentElement.tagName.toLowerCase() !== "script")
+                if (node.lastHandled !== downloadCount) {
+                    handle(node, "nodeValue")
+                    node.lastHandled = downloadCount
+                }
+            break
     }
 }
-function handleText(textNode) {
-    if(textNode.isHandeled != updateCount) {
-        let v = translateText(textNode.nodeValue)
-        if(textNode.nodeValue !== v)
-            textNode.nodeValue = v;
-        textNode.isHandeled = updateCount;
+
+function handle(node, key) {
+    if (node[key]) {
+        const v = translateText(node[key])
+        if (node[key] !== v)
+            node[key] = v
     }
 }
-function handleTitle(node) {
-    if(node.isHandeled != updateCount){
-        if(node.title) {
-            let v = translateText(node.title)
-            if(node.title !== v)
-                node.title = v;
-            node.isHandeled = updateCount;
+
+function translateText(line) {
+    let stripped = line // Prevents recursive TLs
+    for (let key of sorted)
+        if (stripped.includes(key)) {
+            const regex = new RegExp(key)
+            regex.global = true
+
+            line = line.replace(regex, transList[key])
+            stripped = stripped.replace(regex, "") 
         }
-        if(node.label) {
-            let v = translateText(node.label)
-            if(node.label !== v)
-                node.label = v;
-            node.isHandeled = updateCount;
-        }
-    }
+    return line
 }
-function translateText(v) {
-    let prevProp = [""];
-    for (var prop of Object.keys(transList).sort((a,b) => b.length - a.length)) {
-        var strippedV = v;
-        for (var i = 0; i < prevProp.length; i++) 
-            strippedV = strippedV.replace(prevProp[i], "");
-        if (/*transList.hasOwnProperty(prop) && v.includes(prop) && */ strippedV.includes(prop)) {
-            v = v.replace(prop, transList[prop])
-            prevProp.push(prop);
-        }
-    }
-    return v;
-}
+
 function download(url, callback) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
+    const xmlhttp = new XMLHttpRequest()
+    xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == XMLHttpRequest.DONE && xmlhttp.status == 200) {
-            if(callback)
-                callback(JSON.parse(xmlhttp.responseText));
+            if (callback)
+                callback(JSON.parse(xmlhttp.responseText))
 
-            let newData = JSON.parse(xmlhttp.responseText);
-            Object.keys(newData).forEach((k) => transList[k] = newData[k]);
-            if(updateCount == 2)
-                translate();
-            updateCount++;
+            const newData = JSON.parse(xmlhttp.responseText)
+            Object.entries(newData).forEach((k) => transList[k[0]] = k[1])
+            sorted = Object.keys(transList).sort((a, b) => b.length - a.length)
+
+            if (downloadCount == 2)
+                translate()
+            downloadCount++
         }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
+    }
+    xmlhttp.open("GET", url, true)
+    xmlhttp.send()
 }
 
-download('https://raw.githubusercontent.com/KC3Kai/kc3-translations/master/data/en/items.json', (data) => window.gItems && gItems.forEach((a) => a.name = data[a.name]||a.name));
-download('https://raw.githubusercontent.com/KC3Kai/kc3-translations/master/data/en/ships.json', (data) => window.gShips && gShips.forEach((a) => Object.keys(data).sort((a,b) => b.length - a.length).forEach((b) => a.name = a.name.replace(b, data[b]))));
-download('https://raw.githubusercontent.com/Tibowl/DeckbuilderTL/master/Translations.json?v='+new Date().getTime());
+download('https://raw.githubusercontent.com/KC3Kai/kc3-translations/master/data/en/items.json', (data) => window.gItems && gItems.forEach((a) => a.name = data[a.name] || a.name))
+download('https://raw.githubusercontent.com/KC3Kai/kc3-translations/master/data/en/ships.json', (data) => window.gShips && gShips.forEach((a) => Object.keys(data).sort((a, b) => b.length - a.length).forEach((b) => a.name = a.name.replace(b, data[b]))))
+download('https://raw.githubusercontent.com/Tibowl/DeckbuilderTL/master/Translations.json?v=' + new Date().getTime())
